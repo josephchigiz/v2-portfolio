9b25825f06381413edf0944df486c04b1de6de2c
josephchigiz <josephochego@gmail.com>
2024-04-12 14:05:36 +0530

    add lets connect sidebar

--- v2new_portfolio/scripts/app.js
+++ v2new_portfolio/scripts/app.js
@@ -1,5 +1,16 @@
-function myFunction() {
-  var element = document.getElementById("abtBounce");
-  element.classList.add(" animate__animated");
-  element.classList.add(" animate__rubberBand");
-} 
+// function myFunction() {
+//   var element = document.getElementById("abtBounce");
+//   element.classList.add(" animate__animated");
+//   element.classList.add(" animate__rubberBand");
+// }
+
+// var elements = Array.prototype.slice.call(
+//   document.getElementById('v1-img')
+// );
+
+// elements.forEach(function(element) {
+//   new WhenInViewport(element, function(elementInViewport) {
+//     elementInViewport.classList.add('animate__animated');
+//     elementInViewport.classList.add('animate__fadeInLeftBig');
+//   });
+// });
